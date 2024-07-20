72bca3c00fcfd41035d4af493b0ce8db44cfb421
Nicholas Gulachek <nsgulachek@gmail.com>
2024-07-20 09:03:19 -0500

    Add basic send/receive functionality

--- Sources/MsgStream/MsgStream.swift
+++ Sources/MsgStream/MsgStream.swift
@@ -1,2 +1,174 @@
+import Foundation
+
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public protocol MsgHasMutableBytes {
+    mutating func withUnsafeMutableBytes<ResultType>(_ body: (UnsafeMutableRawBufferPointer) throws -> ResultType) rethrows -> ResultType
+}
+
+public protocol MsgHasBytes {
+    func withUnsafeBytes<ResultType>(_ body: (UnsafeRawBufferPointer) throws -> ResultType) rethrows -> ResultType
+}
+
+extension Data : MsgHasBytes, MsgHasMutableBytes {}
+extension [UInt8] : MsgHasBytes, MsgHasMutableBytes {}
+extension ContiguousArray<UInt8> : MsgHasBytes, MsgHasMutableBytes {}
+extension ArraySlice<UInt8> : MsgHasBytes, MsgHasMutableBytes {}
+
+public protocol MsgSender {
+    func send<T: MsgHasBytes>(_ data: T, recvBufSize: Int) throws -> Void;
+}
+
+public protocol MsgReceiver {
+    func receive<T: MsgHasMutableBytes>(_ data: inout T) throws -> UInt64;
+}
+
+public enum MsgStreamError : Error {
+    case badData
+    case writeFailed(Error?)
+    case readFailed(Error?)
+    case msgTooBig(msgSize: UInt64, bufSize: UInt64)
+}
+
+private func bytesToStore(_ n: UInt64) -> UInt8 {
+    var bytes: UInt8 = 0;
+    var counter = n;
+    while (counter > 0) {
+        bytes += 1;
+        counter /= 0x100;
+    }
+    
+    return bytes;
+}
+
+public struct MsgStreamHeader {
+    private var _buf: [UInt8]
+    
+    var buf: ContiguousBytes {
+        get { self._buf }
+    }
+    
+    init(bufSize: UInt64) {
+        let count = Self.size(forMsgBufSize:bufSize)
+        self._buf = [UInt8].init(repeating:0, count:Int(count))
+        self._buf[0] = count
+    }
+    
+    public static func size(forMsgBufSize bufSize: UInt64) -> UInt8 {
+        return bytesToStore(bufSize) + 1;
+    }
+}
+
+public class StreamMsgSender : MsgSender {
+    let stream: OutputStream;
+    
+    init(stream: OutputStream) {
+        self.stream = stream
+    }
+    
+    public func send<T: MsgHasBytes>(_ data: T, recvBufSize: Int) throws -> Void {
+        try data.withUnsafeBytes() { buf in
+            guard let baseAddress = buf.baseAddress else {
+                throw MsgStreamError.badData
+            }
+            
+            let headerSize = MsgStreamHeader.size(forMsgBufSize:UInt64(recvBufSize))
+            var header = [UInt8].init(repeating:0, count:Int(headerSize))
+            header[0] = headerSize
+            var n = buf.count
+            for i in 1..<Int(headerSize) {
+                header[i] = UInt8(n % 0x100)
+                n /= 0x100
+            }
+            
+            try writeN(stream:self.stream, buf:header, count:header.count)
+            try writeN(stream:self.stream, buf:baseAddress, count:buf.count)
+        }
+    }
+    
+    private func pumpErr() throws -> Void {
+        if let err = self.stream.streamError {
+            throw err
+        }
+    }
+    
+    public func open() throws -> Void {
+        self.stream.open()
+        try self.pumpErr()
+    }
+    
+    public func close() throws -> Void {
+        self.stream.close()
+        try self.pumpErr()
+    }
+}
+
+public class StreamMsgReceiver : MsgReceiver {
+    public let stream: InputStream
+    
+    init(stream: InputStream) {
+        self.stream = stream
+    }
+    
+    public func receive<T: MsgHasMutableBytes>(_ data: inout T) throws -> UInt64 {
+        return try data.withUnsafeMutableBytes() { (buf: UnsafeMutableRawBufferPointer) throws in
+            let headerSize = MsgStreamHeader.size(forMsgBufSize: UInt64(buf.count))
+            var header = [UInt8].init(repeating:0,count:Int(headerSize))
+            
+            guard let hdrPtr = (header.withUnsafeMutableBytes() { buf in buf.baseAddress }) else {
+                throw MsgStreamError.badData
+            }
+            try readN(stream:self.stream, buf:hdrPtr, count:header.count)
+            
+            if header[0] != headerSize {
+                throw MsgStreamError.badData
+            }
+            
+            var msgSize = UInt64(0)
+            var power = UInt64(1)
+            for i in 1..<Int(headerSize) {
+                msgSize += power * UInt64(header[i])
+                power *= 0x100
+            }
+            
+            guard let bPtr = buf.baseAddress else {
+                throw MsgStreamError.badData
+            }
+            
+            try readN(stream:self.stream, buf:bPtr, count:Int(msgSize))
+            return msgSize
+        }
+    }
+}
+
+private func writeN(stream: OutputStream, buf: UnsafeRawPointer, count: Int) throws -> Void {
+    var writeBuf = buf
+    var nLeft = count
+    while nLeft > 0 {
+        let n = stream.write(writeBuf, maxLength: nLeft)
+        if (n < 0) {
+            throw MsgStreamError.writeFailed(stream.streamError)
+        }
+        
+        nLeft -= n
+        writeBuf = writeBuf.advanced(by:n)
+    }
+}
+
+private func readN(stream: InputStream, buf: UnsafeMutableRawPointer, count: Int) throws -> Void {
+    var readBuf = buf
+    var nLeft = count
+    while nLeft > 0 {
+        let n = stream.read(readBuf, maxLength:nLeft)
+        if (n < 0) {
+            throw MsgStreamError.readFailed(stream.streamError)
+        } else if (n == 0) {
+            throw MsgStreamError.badData
+        }
+        
+        nLeft -= n
+        readBuf = readBuf.advanced(by:n)
+    }
+}
--- Tests/MsgStreamTests/MsgStreamTests.swift
+++ Tests/MsgStreamTests/MsgStreamTests.swift
@@ -1,12 +1,224 @@
 import XCTest
+import Foundation
 @testable import MsgStream
 
-final class MsgStreamTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+func headerSizeAssert(bufSize: UInt64, expect: UInt8) -> Void {
+    let actual = MsgStreamHeader.size(forMsgBufSize: bufSize);
+    XCTAssertEqual(actual, expect);
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class MsgStreamTests: XCTestCase {
+    func testHeaderSize0Bytes() {
+        headerSizeAssert(bufSize: 0, expect: 1);
+    }
+    
+    func testHeaderSize2Bytes() {
+        headerSizeAssert(bufSize: 0x01, expect: 2);
+        headerSizeAssert(bufSize: 0xff, expect: 2);
+    }
+    
+    func testHeaderSize3Bytes() {
+        headerSizeAssert(bufSize: 0x0100, expect: 3);
+        headerSizeAssert(bufSize: 0xffff, expect: 3);
+    }
+    
+    func testHeaderSize4Bytes() {
+        headerSizeAssert(bufSize: 0x010000, expect: 4);
+        headerSizeAssert(bufSize: 0xffffff, expect: 4);
+    }
+    
+    func testHeaderSize5Bytes() {
+        headerSizeAssert(bufSize: 0x01000000, expect: 5);
+        headerSizeAssert(bufSize: 0xffffffff, expect: 5);
+    }
+    
+    func testHeaderSize6Bytes() {
+        headerSizeAssert(bufSize: 0x0100000000, expect: 6);
+        headerSizeAssert(bufSize: 0xffffffffff, expect: 6);
+    }
+    
+    func testHeaderSize7Bytes() {
+        headerSizeAssert(bufSize: 0x010000000000, expect: 7);
+        headerSizeAssert(bufSize: 0xffffffffffff, expect: 7);
+    }
+    
+    func testHeaderSize8Bytes() {
+        headerSizeAssert(bufSize: 0x01000000000000, expect: 8);
+        headerSizeAssert(bufSize: 0xffffffffffffff, expect: 8);
+    }
+    
+    func testHeaderSize9Bytes() {
+        headerSizeAssert(bufSize: 0x0100000000000000, expect: 9);
+        headerSizeAssert(bufSize: 0xffffffffffffffff, expect: 9);
+    }
+    
+    func testSendsAndReceivesSingleMessage() throws {
+        let streams = MemStreams()
+        
+        var msg = TestMessage()
+        msg.id = 24
+        msg.type = "test"
+        
+        try msg.send(streams:streams)
+        streams.copyOutputToInput()
+        try msg.assertRecvEqual(streams: streams)
+    }
+    
+    func testSendsAndReceivesTwoMessages() throws {
+        let streams = MemStreams()
+        
+        var msg = TestMessage()
+        msg.id = 24
+        msg.type = "test"
+        
+        try msg.send(streams:streams)
+        try msg.send(streams:streams)
+        streams.copyOutputToInput()
+        try msg.assertRecvEqual(streams: streams)
+        try msg.assertRecvEqual(streams: streams)
+    }
+    
+    func testCanSendData() throws {
+        let buf = Data([1, 2, 3])
+        let streams = MemStreams()
+        try streams.sender.send(buf, recvBufSize: 32)
+        let outData = streams.outputData()
+        XCTAssertEqual(outData, Data([2, 3, 1, 2, 3]))
+    }
+    
+    func testCanSendArray() throws {
+        let buf: [UInt8] = [1, 2, 3]
+        let streams = MemStreams()
+        try streams.sender.send(buf, recvBufSize: 32)
+        let outData = streams.outputData()
+        XCTAssertEqual(outData, Data([2, 3, 1, 2, 3]))
+    }
+    
+    func testCanSendArraySlice() throws {
+        var buf = [UInt8].init(repeating:0, count:32)
+        buf.replaceSubrange(..<3, with: [1, 2, 4])
+        let slice = buf[..<3]
+        
+        let streams = MemStreams()
+        try streams.sender.send(slice, recvBufSize: buf.count)
+        let outData = streams.outputData()
+        XCTAssertEqual(outData, Data([2, 3, 1, 2, 4]))
+    }
+    
+    func testCanSendContiguousArray() throws {
+        let buf = ContiguousArray<UInt8>([3, 2, 1])
+        
+        let streams = MemStreams()
+        try streams.sender.send(buf, recvBufSize: 32)
+        let outData = streams.outputData()
+        XCTAssertEqual(outData, Data([2, 3, 3, 2, 1]))
+    }
+    
+    func testCanRecvData() throws {
+        let streams = MemStreams()
+        streams.copyToInput(data:Data([2, 3, 3, 2, 1]))
+        
+        var buf = Data(count:32)
+        let n = try streams.receiver.receive(&buf)
+        XCTAssertEqual(buf[..<n], Data([3, 2, 1]))
+    }
+    
+    func testCanRecvArray() throws {
+        let streams = MemStreams()
+        streams.copyToInput(data:Data([2, 4, 4, 3, 2, 1]))
+        
+        var buf = [UInt8].init(repeating:0, count:32)
+        let n = try streams.receiver.receive(&buf)
+        XCTAssertEqual(buf[..<Int(n)], [4, 3, 2, 1])
+    }
+    
+    func testCanRecvArraySlice() throws {
+        let streams = MemStreams()
+        streams.copyToInput(data:Data([2, 4, 4, 3, 2, 1]))
+        
+        var buf = [UInt8].init(repeating:0, count:64)
+        let n = try streams.receiver.receive(&buf[32...])
+        XCTAssertEqual(buf[32..<32+Int(n)], [4, 3, 2, 1])
+    }
+    
+    func testCanRecvContiguousArray() throws {
+        let streams = MemStreams()
+        streams.copyToInput(data:Data([2, 5, 1, 2, 3, 4, 5]))
+        
+        var buf = ContiguousArray<UInt8>(repeating:0,count:32)
+        let n = try streams.receiver.receive(&buf)
+        XCTAssertEqual(buf[..<Int(n)], [1, 2, 3, 4, 5])
+    }
+}
+
+struct TestMessage : Codable, Equatable {
+    public var id: Int
+    public var type: String
+    private static let bufSize = 2048
+    
+    init() {
+        self.id = 0
+        self.type = ""
+    }
+    
+    public func toJSON() throws -> Data {
+        let encoder = JSONEncoder()
+        return try encoder.encode(self)
+    }
+    
+    public static func fromJSON(json: Data) throws -> TestMessage {
+        let decoder = JSONDecoder()
+        return try decoder.decode(Self.self, from:json)
+    }
+    
+    public func send(streams: MemStreams) throws -> Void {
+        let sendJson = try self.toJSON()
+        try streams.sender.send(sendJson, recvBufSize: Self.bufSize)
+    }
+    
+    public func assertRecvEqual(streams: MemStreams) throws -> Void {
+        var buf = Data(count:Self.bufSize)
+        let n = try streams.receiver.receive(&buf)
+        buf.count = Int(n)
+        let msgRecv = try TestMessage.fromJSON(json:buf)
+        XCTAssertEqual(self, msgRecv)
+    }
+}
+
+class MemStreams {
+    public var input: InputStream
+    public var output: OutputStream
+    public var sender: StreamMsgSender;
+    public var receiver: StreamMsgReceiver;
+    
+    init() {
+        self.output = OutputStream.toMemory()
+        self.sender = StreamMsgSender(stream: self.output)
+        self.output.open()
+        
+        self.input = InputStream(data:Data())
+        self.receiver = StreamMsgReceiver(stream: self.input)
+        self.input.open()
+    }
+    
+    public func outputData() -> Data {
+        self.output.property(forKey:Stream.PropertyKey.dataWrittenToMemoryStreamKey) as! Data
+    }
+    
+    public func copyOutputToInput() -> Void {
+        self.input.close()
+        let data = self.outputData()
+        self.copyToInput(data:data)
+    }
+    
+    public func copyToInput(data: Data) -> Void {
+        self.input = InputStream(data:data)
+        self.receiver = StreamMsgReceiver(stream: self.input)
+        self.input.open()
+    }
+    
+    deinit {
+        self.output.close()
+        self.input.close()
     }
 }
